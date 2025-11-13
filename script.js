c801caeab8a36f5fae235e5bf504df359d0375f2
Skayzinxx1 <matheusgeo50@gmail.com>
2025-11-13 15:27:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Efeito de aparecer ao rolar
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+document.querySelectorAll('.fade').forEach(el => observer.observe(el));
